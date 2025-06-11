4ffb2af726f8f8c336d68f333feb2f0cb2b5e6c5
Student <ay879018@gmail.com>
2025-06-11 11:13:50 +0530

    hello

--- day2(adv).py
+++ day2(adv).py
@@ -1,2 +1,21 @@
-#
-mera naam alok hai
+class Apple_design:
+    count = 0
+    def __init__(self, cpu, ram):  #constructer
+        self.cpu = cpu
+        self.ram = ram
+
+
+    def mobile(self):
+        print(type(self))
+        print("this apple phhone1")
+        print(self.cpu, self.ram)
+
+
+# create a object
+m1 = Apple_design(3.5, 8)
+m2 = Apple_design(4.5, 16)
+m1.mobile()
+m2.mobile()
+print(id(m1))
+print(id(m2))
+
