1327a96d179242bd4984b823c7de50b5d1bdad3f
Student <ay879018@gmail.com>
2025-06-11 12:11:34 +0530

    methods

--- day2(adv4).py
+++ day2(adv4).py
@@ -1,18 +1,35 @@
-class Number:
-    evens = []
-    odds = []
-    def __init__(self, num):
-        self.num = num
-        if num % 2 ==0:
-            Number.evens.append(num)
-        else:
-            Number.odds.append(num)
-N1 = Number(21)
-N2 = Number(32)
-N3 = Number(43)
-N4 = Number(54)
-N5 = Number(65)
+# class Number:
+#     evens = []
+#     odds = []
+#     def __init__(self, num):
+#         self.num = num
+#         if num % 2 ==0:
+#             Number.evens.append(num)
+#         else:
+#             Number.odds.append(num)
+# N1 = Number(21)
+# N2 = Number(32)
+# N3 = Number(43)
+# N4 = Number(54)
+# N5 = Number(65)
+#
+# print("even Numbers are:", Number.evens)
+# print("odd Numbers are:", Number.odds)
 
-print("even Numbers are:", Number.evens)
-print("odd Numbers are:", Number.odds)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#     def __str__(self):
+#         return F"The name is {self.name} and age is {self.age}"
+#
+# p = Person("Lemon",4)
+# print(p)
+#
+
+
 
