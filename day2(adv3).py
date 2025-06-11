a5cce2cf8234a320d11ab63af105f4c0abfcd68b
Student <ay879018@gmail.com>
2025-06-11 11:36:15 +0530

    ni hao fine shyt

--- day2(adv3).py
+++ day2(adv3).py
@@ -1,67 +1,14 @@
-class Apple_design:
-    count = 0
-    def __init__(self, cpu, ram):  #constructer
-        self.cpu = cpu
-        self.ram = ram
+class Employee:
+    office_name = "Seedhe Maut"
+    def __init__(self, name, destination):
+        self.name = name
+        self.destination = destination
 
-
-    def mobile(self):
-        print(type(self))
-        print("this apple phhone1")
-        print(self.cpu, self.ram)
-
-
-# create a object
-m1 = Apple_design(3.5, 8)
-m2 = Apple_design(4.5, 16)
-m1.mobile()
-m2.mobile()
-print(id(m1))
-print(id(m2))
-
-
-
-
-
-
-
-#
-#
-# class c_omputer:
-#     company = "dell"
-#     # count = 2
-#     def __init__(self, ram, processor, rom):
-#         self.ram = ram
-#         self.processor = processor
-#         self.rom = rom
-#     def system(self):
-#         print("computer is running")
-#         print(self.ram, self.processor, self.rom)
-# c1 = c_omputer(8,10, 100)
-# c2 = c_omputer(16,90, 100)
-# c3 = c_omputer(16,100, 100)
-# c1.system()
-# c2.system()
-# c3.system()
-
-
-
-
-# car design
-
-class Car_Design:
-    def __init__(self, model, color, year, ):
-        self.model = model
-        self.color = color
-        self.year = year
-
-
-    def car_info(self):
-        print("Model:", self.model,",Color:",self.color,",Size:", self.year)
-
-
-c1 = Car_Design("Hyundai","Blue",2002)
-c2 = Car_Design("Marutii", "Red",2001)
-c1.car_info()
-c2.car_info()
-
+    def show_details(self):
+        print(f"Company Name: {self.office_name}")
+        print("Name:",self.name)
+        print("Destination:",self.destination)
+emp1 = Employee("Calm","best in the biz")
+emp2 = Employee("Encore", "kavi")
+emp1.show_details()
+emp2.show_details()
