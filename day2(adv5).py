7b63c222b18253694fa7491ad047900c9e0edc5f
Student <ay879018@gmail.com>
2025-06-11 12:22:48 +0530

    methods

--- day2(adv5).py
+++ day2(adv5).py
@@ -1,16 +1,37 @@
 # use of decorator
-class Student:
-    counter = 0
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
-        Student.counter += 1
-    def msg(self):
-        print("Hello"+self.name+"you got",self.marks,"% marks")
-    @classmethod #function decorator
-    def object_count(cls):
-        return cls.counter
-s1 = Student("Alok",66)
-s2 = Student("abc",32)
-print(Student.object_count())
-print(s1.object_count())
+# class Student:
+#     counter = 0
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#         Student.counter += 1
+#     def msg(self):
+#         print("Hello"+self.name+"you got",self.marks,"% marks")
+#     @classmethod #function decorator
+#     def object_count(cls):
+#         return cls.counter
+# s1 = Student("Alok",66)
+# s2 = Student("abc",32)
+# print(Student.object_count())
+# print(s1.object_count())
+
+#```````````````````````````````````````````````````````````````````````````````
+
+class MathsOperations:
+    @staticmethod
+    def add_num(a,b):
+        return a+b
+    @staticmethod
+    def sub(a,b):
+        return a-b
+
+obj = MathsOperations()
+result_add = MathsOperations.add_num(10,20) # call by className
+result_sub = obj.sub(10,20) #call by objectName
+
+print(f"Addition: {result_add}")
+print(f"Subtraction: {result_sub}")
+
+
+
+
