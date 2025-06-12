000073f8fb9a53b3b586c3ea9089b5d54748e33c
Student <ay879018@gmail.com>
2025-06-12 09:25:31 +0530

    methods

--- day2(adv5).py
+++ day2(adv5).py
@@ -17,20 +17,56 @@
 
 #```````````````````````````````````````````````````````````````````````````````
 
-class MathsOperations:
-    @staticmethod
-    def add_num(a,b):
-        return a+b
-    @staticmethod
-    def sub(a,b):
-        return a-b
+# class MathsOperations:
+#     @staticmethod
+#     def add_num(a,b):
+#         return a+b
+#     @staticmethod
+#     def sub(a,b):
+#         return a-b
+#
+# obj = MathsOperations()
+# result_add = MathsOperations.add_num(10,20) # call by className
+# result_sub = obj.sub(10,20) #call by objectName
+#
+# print(f"Addition: {result_add}")
+# print(f"Subtraction: {result_sub}")
 
-obj = MathsOperations()
-result_add = MathsOperations.add_num(10,20) # call by className
-result_sub = obj.sub(10,20) #call by objectName
 
-print(f"Addition: {result_add}")
-print(f"Subtraction: {result_sub}")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# class Student:
+#     def __init__(self):
+#         print("Default constructor called")
+#
+#     def __init__(self, name, marks, age):
+#         self.name = name
+#         self.marks = marks
+#         self.age = age
+#         print("Parameterized constructor called")
+#     def show_info(self ):
+#         print("Name", self.name)
+#         print("Marks", self.marks)
+#         print("Age", self.age)
+#     def __del__(self):
+#         print("Destructors called")
+#
+#
+# s1 = Student() #default parameter is being called
+# s2 = Student("Rohit", 95.5, 19)
+# s1.show_info()
+# s2.show_info()
+#
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
 
 
 
